fd576406fa4cfcbbcfd22901a8512a5d66e379b9
Faizan <149582509+faiz4n@users.noreply.github.com>
2024-05-18 23:44:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,10 +2,21 @@ let boxes = document.querySelectorAll(".box");
 let winnerName = document.querySelector("h2");
 let resetButton = document.querySelector(".reset");
 
-resetButton.addEventListener("click", ()=>{
-    
-        location.reload();
-    }
+resetButton.addEventListener("click", () => {
+
+    boxes.forEach((box) => {
+        box.innerText = "";
+        for (let patterns of winPatterns) {
+            let vp1 = boxes[patterns[0]];
+            let vp2 = boxes[patterns[1]];
+            let vp3 = boxes[patterns[2]];
+
+            vp1.classList.remove("winnerclass")
+            vp2.classList.remove("winnerclass")
+            vp3.classList.remove("winnerclass")
+        }
+    })
+}
 )
 let winner;
 
@@ -26,6 +37,7 @@ boxes.forEach((box) => {
 
         }
         checkWinner();
+
     })
 
 })
@@ -51,7 +63,7 @@ checkWinner = () => {
         let vp1 = boxes[patterns[0]];
         let vp2 = boxes[patterns[1]];
         let vp3 = boxes[patterns[2]];
-        
+
 
         if (vp1.innerText != "" && vp2.innerText != "" && vp3.innerText != "") {
             if (vp1.innerText === vp2.innerText && vp2.innerText === vp3.innerText) {
@@ -60,18 +72,18 @@ checkWinner = () => {
                     box.disabled = true;
                     let winner = vp1.innerText === "X" ? "Player 1" : "Player 2";
 
-                    winnerName.innerText=`Winner is : ${winner}`;
-                    vp1.style.backgroundColor="red";
-                    vp1.style.color="white";
+                    winnerName.innerText = `Winner is : ${winner}`;
 
-                    vp2.style.backgroundColor="red";
-                    vp2.style.color="white";
+                    vp1.classList.add("winnerclass")
+                    vp2.classList.add("winnerclass")
+                    vp3.classList.add("winnerclass")
 
-                    vp3.style.backgroundColor="red";
-                    vp3.style.color="white";
+                    for (box of boxes) {
+                        box.style.pointerEvents = "none";
+                    }
 
-                    
-                    
+
+
 
                 })
             }
